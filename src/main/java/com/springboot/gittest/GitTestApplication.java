c4fefe87d81ac00d28b982cf2cddd405572f9381
ZuoJiaQi <ZuoJiaQi@qq.com>
2023-09-14 14:36:51 +0800

    second commit test

--- src/main/java/com/springboot/gittest/GitTestApplication.java
+++ src/main/java/com/springboot/gittest/GitTestApplication.java
@@ -1,14 +1,14 @@
 package com.springboot.gittest;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitTestApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(GitTestApplication.class, args);
         System.out.println("hello, Git");
+        System.out.println("测试");
+        System.out.println("好好好");
     }
 
 }
