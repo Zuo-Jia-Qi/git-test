dad5e225b080590cdba5f09f7551cd72909e10a3
ZuoJiaQi <ZuoJiaQi@qq.com>
2023-09-14 14:46:56 +0800

    third commit test

--- src/main/java/com/springboot/gittest/GitTestApplication.java
+++ src/main/java/com/springboot/gittest/GitTestApplication.java
@@ -7,7 +7,10 @@ public class GitTestApplication {
 
     public static void main(String[] args) {
         System.out.println("hello, Git");
-        System.out.println("测试");
+        System.out.println("hello, Git");
+        System.out.println("hello, Git");
+        System.out.println("hello, Git");
+        System.out.println("hello, Git");
         System.out.println("好好好");
     }
 
